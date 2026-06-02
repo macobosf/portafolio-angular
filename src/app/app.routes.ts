436029c92823b42355ef54ceea580c7d5b6c7d93
macobosf <marcocobos15@gmail.com>
2026-06-02 15:09:52 -0500

    feat: configura rutas con lazy loading, guards y providers de Firebase

---
+++ src/app/app.routes.ts
@@ -0,0 +1,71 @@
+import { Routes } from '@angular/router';
+import { authGuard, programmerGuard } from './core/auth.guard';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'programador/:slug',
+    loadComponent: () =>
+      import('./pages/programmer-profile/programmer-profile.component').then(
+        (m) => m.ProgrammerProfileComponent,
+      ),
+  },
+  {
+    path: 'proyectos',
+    loadComponent: () =>
+      import('./pages/projects/projects.component').then(
+        (m) => m.ProjectsComponent,
+      ),
+  },
+  {
+    path: 'proyectos/:slug',
+    loadComponent: () =>
+      import('./pages/project-detail/project-detail.component').then(
+        (m) => m.ProjectDetailComponent,
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'registro',
+    loadComponent: () =>
+      import('./pages/register/register.component').then(
+        (m) => m.RegisterComponent,
+      ),
+  },
+  {
+    path: 'solicitud',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./pages/contact-request/contact-request.component').then(
+        (m) => m.ContactRequestComponent,
+      ),
+  },
+  {
+    path: 'dashboard/programador',
+    canActivate: [programmerGuard],
+    loadComponent: () =>
+      import(
+        './pages/dashboard-programmer/dashboard-programmer.component'
+      ).then((m) => m.DashboardProgrammerComponent),
+  },
+  {
+    path: 'dashboard/usuario',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./pages/dashboard-user/dashboard-user.component').then(
+        (m) => m.DashboardUserComponent,
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
