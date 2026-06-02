5fba077ac8795551ca99a94732eb9f89cb52e267
macobosf <marcocobos15@gmail.com>
2026-06-02 15:09:40 -0500

    feat: agrega archivos de entorno con configuración de Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBxZngX-7eQ4NaIOWdEFF9jzXIntJ86QcE',
+    authDomain: 'portafolio-54995.firebaseapp.com',
+    projectId: 'portafolio-54995',
+    storageBucket: 'portafolio-54995.firebasestorage.app',
+    messagingSenderId: '430812633813',
+    appId: '1:430812633813:web:337ba9bb5636d32d68205c',
+    measurementId: 'G-VG16KTMQEZ',
+  },
+};
