1d1c83904dcca2c6b77a31c3671323772523ce7d
macobosf <marcocobos15@gmail.com>
2026-06-02 15:09:07 -0500

    feat: bootstrapping de la aplicación Angular

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  templateUrl: './app.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class App {}
